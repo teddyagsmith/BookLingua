bf455d810fc366c1ba19beb8f91712d382cef6b7
teddyagsmith <teddy@teddyagsmith.com>
2026-02-25 22:15:40 +0000

    Delete translateabook 3 directory

--- translateabook 3/app/api/inngest/route.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from 'inngest/next'
-import { inngest } from '@/lib/inngest'
-import { translateBook } from '@/lib/translate-job'
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [translateBook],
-})
